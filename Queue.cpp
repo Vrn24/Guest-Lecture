1b0155b2dbf6c8c9225b6e8196b57ee4c59fd0fa
Vrn24 <112547439+Vrn24@users.noreply.github.com>
2023-10-23 15:41:39 +0530

    Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -1,3 +1,82 @@
+/*22070123129*/
+/*Varun Pagote*/
+/*Queue Implimentation*/
+
+#include <iostream>
+
+const int MAX_SIZE = 100; // Maximum size of the queue
+
+class Queue {
+private:
+    int front, rear;
+    int arr[MAX_SIZE];
+
+public:
+    Queue() {
+        front = -1;
+        rear = -1;
+    }
+
+    // Function to check if the queue is empty
+    bool isEmpty() {
+        return (front == -1 && rear == -1);
+    }
+
+    // Function to check if the queue is full
+    bool isFull() {
+        return (rear + 1) % MAX_SIZE == front;
+    }
+
+    // Function to add an element to the queue
+    void enqueue(int value) {
+        if (isFull()) {
+            std::cout << "Queue is full. Cannot enqueue." << std::endl;
+            return;
+        } else if (isEmpty()) {
+            front = rear = 0;
+        } else {
+            rear = (rear + 1) % MAX_SIZE;
+        }
+        arr[rear] = value;
+    }
+
+    // Function to remove an element from the queue
+    void dequeue() {
+        if (isEmpty()) {
+            std::cout << "Queue is empty. Cannot dequeue." << std::endl;
+            return;
+        } else if (front == rear) {
+            front = rear = -1;
+        } else {
+            front = (front + 1) % MAX_SIZE;
+        }
+    }
+
+    // Function to get the front element of the queue
+    int getFront() {
+        if (isEmpty()) {
+            std::cout << "Queue is empty." << std::endl;
+            return -1;
+        }
+        return arr[front];
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    std::cout << "Front element: " << q.getFront() << std::endl;
+
+    q.dequeue();
+    std::cout << "Front element after dequeue: " << q.getFront() << std::endl;
+
+    return 0;
+}
+
 #include <iostream>
 
 const int MAX_SIZE = 100; // Maximum size of the queue
